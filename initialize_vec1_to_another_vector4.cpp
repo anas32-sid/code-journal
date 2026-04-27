c34fe89e0bdca326b74fb8897c59780c425ebc7a
mohd. anas <anas9650731343@gmail.com>
2026-04-27 22:54:45 +0530

    Add files via upload

---
+++ initialize_vec1_to_another_vector4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class solution{
+public:
+vector<int> run(vector<int>&v){
+vector<int> v2=v;
+return v2;
+}
+};
+int main(){
+    solution obj;
+    vector<int> v1 = {1, 2, 3,4, 5, 6, 7,8,9};
+    vector<int>result=obj.run(v1);
+
+}
